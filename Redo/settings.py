b81e386da3e6fd767f98d450ebbaa0825e379a9b
EmcioN <villly1990@gmail.com>
2023-09-02 19:44:10 +0000

    Change settings.py now images are loading

--- Redo/settings.py
+++ Redo/settings.py
@@ -179,4 +179,4 @@ AWS_QUERYSTRING_AUTH = False
 
 # Media files: Uploaded by users (e.g., images)
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_STORAGE_BUCKET_NAME}/'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
